d07066f4160ba3b8217ffe4d450961dc06cf3308
ShaineHo <yhoo0020@student.monash.edu>
2021-10-07 18:07:15 +0800

    edit key-crop_yields.R

---
+++ R/key_crop_yields.R
@@ -0,0 +1,18 @@
+#' The data was collected from Tidy Tuesday Git Repository
+#'
+#'
+#'
+#'
+#' @format A data frame with 53948 rows and 10 variables:
+#' \describe{
+#' \item{Entity}{Country}
+#' \item{tonnes_per_hectare}{yield}
+#'
+#'...
+#'}
+#'
+#' @source 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv'
+#'
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
